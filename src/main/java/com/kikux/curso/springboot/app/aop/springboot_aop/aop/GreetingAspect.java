6c7dbe8546f852f7466e03a3f1bab3bedc21e6d4
kikux-ubuntu <i.am.kike@gmail.com>
2025-05-03 01:50:18 -0600

    adding after

--- src/main/java/com/kikux/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
+++ src/main/java/com/kikux/curso/springboot/app/aop/springboot_aop/aop/GreetingAspect.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.SourceLocation;
@@ -17,11 +18,20 @@ public class GreetingAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution (String com.kikux.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))")
+    // @Before("execution (* com.kikux.curso.springboot.app.aop.springboot_aop.services.GreetingService.sayHello(..))") //any return
+    // @Before("execution (* *.*(..))") //any packaga any class any return
+    // @Before("execution (* com.kikux.curso.springboot.app.aop.springboot_aop..*.*(..))") //any class in project
+    @Before("execution (* com.kikux.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))") //any return
     public void loggerBefore(JoinPoint joinPoint) {
         Signature signature = joinPoint.getSignature();        
         String args = Arrays.toString(joinPoint.getArgs());
-        SourceLocation sourceLocation = joinPoint.getSourceLocation();
-        logger.info("Before entering to: " + signature + " with args: " + args + " and source: " + sourceLocation);
+        logger.info("Before entering to: " + signature + " with args: " + args);
+    }
+
+    @After("execution (* com.kikux.curso.springboot.app.aop.springboot_aop.services.GreetingService.*(..))") //any return
+    public void loggerAfter(JoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();        
+        String args = Arrays.toString(joinPoint.getArgs());
+        logger.info("After: " + signature + " with args: " + args);
     }
 }
